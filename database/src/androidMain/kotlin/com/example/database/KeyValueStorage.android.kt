04d5d90caecd7f6d5ba548bb20c1c229e0db6434
Deepak Goyal <deepakgoyal@getpowerplay.in>
2026-03-16 15:46:17 +0530

    Fixed : multiplatform task for maven publish

--- database/src/androidMain/kotlin/com/example/database/KeyValueStorage.android.kt
+++ database/src/androidMain/kotlin/com/example/database/KeyValueStorage.android.kt
@@ -1,10 +1,11 @@
 package com.example.database
-//
-//actual object KeyValueStorage {
-//
-//    actual inline operator fun <reified T>get(key: String): T{
-//        TODO("Not yet implemented")
-//    }
-//    actual operator fun set(key: String, value: Any) {
-//    }
-//}
+
+actual object KeyValueStorage {
+
+    actual inline operator fun <reified T>get(key: String): T{
+        TODO("Not yet implemented")
+    }
+
+    actual operator fun set(key: String, value: Any) {
+    }
+}
--- database/src/iosMain/kotlin/com/example/database/KeyValueStorage.ios.kt
+++ database/src/iosMain/kotlin/com/example/database/KeyValueStorage.ios.kt
@@ -1,11 +1,12 @@
 package com.example.database
 
-//actual object KeyValueStorage {
-//
-//    actual inline operator fun <reified T>get(key: String): T{
-//        TODO("Not yet implemented")
-//    }
-//    actual operator fun set(key: String, value: Any) {
-//
-//    }
-//}
+actual object KeyValueStorage {
+
+    actual inline operator fun <reified T>get(key: String): T{
+        TODO("Not yet implemented")
+    }
+
+    actual operator fun set(key: String, value: Any) {
+
+    }
+}
