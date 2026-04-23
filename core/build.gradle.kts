629e8c9342c0ca5b37d650f1203c18176382500c
kanhaiya01 <kanhaiya@getpowerplay.in>
2026-04-23 14:12:02 +0530

    remove ok http dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -30,7 +30,6 @@ kotlin {
         androidMain{
             dependencies {
                 implementation(libs.androidx.core)
-                implementation(libs.okhttp)
             }
         }
     }
